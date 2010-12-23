13caf9c5fc1b4275b05a7e82d49e852cace6295e
royarzun <royarzun@gmail.com>
2010-12-23 17:17:28 -0300

    Add exclusive model function in THapradUtils

--- src/THapradUtils.h
+++ src/THapradUtils.h
@@ -12,8 +12,10 @@ namespace HapradUtils {
                             Double_t pt2, Double_t mx2,
                             Double_t pl, Double_t& H1z,
                             Double_t& H2z, Double_t& H3z,
-                            Double_t& H4z)
+                            Double_t& H4z);
     void dfint(Int_t narg, Double_t *arg, Double_t *nent, Double_t *ent, Double_t *table);
+    void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t st,
+                        Double_t sl, Double_t stt, Double_t stl, Double_t stlp);
 
 }
 
