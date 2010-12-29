90f6f52cb7ba42737b84d6d4905c5ca559d8509c
royarzun <royarzun@gmail.com>
2010-12-29 09:15:15 -0300

    Add the TExclusiveModel header

---
+++ TExclusiveModel.h
@@ -0,0 +1,8 @@
+#ifndef TEXCLUSIVEMODEL_H
+#define TEXCLUSIVEMODEL_H
+
+
+void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t st,
+                    Double_t sl, Double_t stt, Double_t stl, Double_t stlp);
+
+#endif
