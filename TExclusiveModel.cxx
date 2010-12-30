04e0e4dd78fa33b4b830b844f9b4df7bb086b8eb
royarzun <royarzun@gmail.com>
2010-12-30 14:12:29 -0300

    Prototype of function edited

--- TExclusiveModel.cxx
+++ TExclusiveModel.cxx
@@ -3,9 +3,10 @@
 #include <iostream>
 #include "TMath.h"
 
-void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t st,
-                    Double_t sl, Double_t stt, Double_t stl, Double_t stlp)
+void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t &st,
+                    Double_t& sl, Double_t& stt, Double_t& stl, Double_t& stlp)
 {
+    Double_t q2,w;
     const Int_t nq = 18;
     const Int_t nw = 47;
     const Int_t nt = 61;
--- TExclusiveModel.h
+++ TExclusiveModel.h
@@ -2,7 +2,7 @@
 #define TEXCLUSIVEMODEL_H
 
 
-void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t st,
-                    Double_t sl, Double_t stt, Double_t stl, Double_t stlp);
+void ExclusiveModel(Double_t q2m, Double_t wm, Double_t csthcm, Double_t &st,
+                    Double_t &sl, Double_t &stt, Double_t &stl, Double_t &stlp);
 
 #endif
